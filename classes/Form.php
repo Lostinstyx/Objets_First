780c222765f42320d9fa4f5550165d7db8b2166f
Lostinstyx <courbois.alexandre76440@gmail.com>
2020-01-28 23:15:42 +0100

    try ini

--- classes/Form.php
+++ classes/Form.php
@@ -4,6 +4,17 @@
 class Form
 {
     private  $_method = "post";
+    private $_label = "";
+    private $_input = "";
+    private $_name = "";
+    private $_placeholder = "";
+    private $_type = "";
+    private $_for = "";
+    private $_class = "";
+    private $_id = "";
+    private $_value = "";
+    private $_row = "";
+    private $_cols = "";
     private $_url = "";
     private $_upLoadFile = false;
     private $_iniFile;
@@ -27,6 +38,38 @@ class Form
 
         return $html;
     }
+
+    public function labelForm() : string
+    {
+        $html = '<label ';
+        $html .= 'for="'.$this->_for.'" ';
+        $html .= 'class="'.$this->_class.'" ';
+        $html .= 'id="'.$this->_id.'" ';
+        $html .= '>'.$this->_label.'</label>';
+
+        return $html;
+    }
+
+    public function inputForm() : string
+    {
+        $html = '<input ';
+        $html .= 'type="'.$this->_type.'" ';
+        $html .= 'name="'.$this->_name.'" ';
+        $html .= 'placeholder="'.$this->_placeholder.'" ';
+        $html .= 'value="'.$this->_value.'" ';
+        $html .= 'class="'.$this->_class.'" ';
+        $html .= 'id="'.$this->_id.'" ';
+        $html .= 'row="'.$this->_row.'" ';
+        $html .= 'cals="'.$this->_cols.'" ';
+        $html .= '>'.$this->_input.'</input>';
+
+
+        return $html;
+    }
+    
 }
 
+
 // Travailler la dessus et faire un tableau avec tout ce qui existe label input etc... (lire le tout avec un foreach)
+?>
+
