f202d09efd055754fe4ecb95f60d9dc8588af139
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-01-28 11:36:37 +0100

    ajout constructor

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -3,12 +3,25 @@
 
 class Voiture
 {
-    public $marque;
-    public $modele;
-    public $couleur;
-    public $dimensions = [0,0,0];
-    public $masse;
-    public $vitesse = 0;
+    public $_marque;
+    public $_modele;
+    public $_couleur;
+    public $_dimensions = [0,0,0];
+    public $_masse;
+    public $_vitesse = 0;
 
+    public function __construct(string $marque, string $modele,string $couleur, array $dimensions, int $masse,int $vitesse)
+    {
+        $this->_marque = $marque;
+        $this->_modele = $modele;
+        $this->_couleur = $couleur;
+        $this->_dimensions = $dimensions;
+        $this->_masse = $masse;
+        $this->_vitesse = $vitesse;
+    }
 
+    public function afficherMessage()
+    {
+        echo "Je suis Michel";
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+require('classes/Voiture.php');
+require('functions/debug.php');
+
+
+/*$voiture1 = new Voiture();
+
+debug($voiture1);
+
+$voiture1->couleur = 'rouge';
+$voiture1->masse = 1000;
+$voiture1->modele = 'R14';
+$voiture1->dimensions = [2000,1500,800];
+$dimensions = $voiture1->dimensions;*/
+
+$voiture = new Voiture('Dacia','Sandero','rouge',[5000,3000,1500],'2000','0');
+
+debug($voiture);
+
